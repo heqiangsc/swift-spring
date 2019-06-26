cd438af3cde0b8157cced3a0c4007211ef25cef0
heqiang <heqiang@kuainiugroup.com>
2019-06-26 17:32:56 +0800

    init

---
+++ src/main/java/com/jialing/swift/swiftspring/SwiftSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jialing.swift.swiftspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftSpringApplication.class, args);
+    }
+
+}
